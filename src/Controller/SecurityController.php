6791303eb5b4428225512ca9d93d276bb94fa07c
devDzign <chabour.mourad87@gmail.com>
2020-12-01 00:12:25 +0100

    feature(registration):
    * controller, form, template, tests

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
--- tests/Functional/LoginTest.php
+++ tests/Functional/LoginTest.php
@@ -21,7 +21,7 @@ class LoginTest extends WebTestCase
         $crawler = $client->request(Request::METHOD_GET, $router->generate("app_login"));
 
         $form = $crawler->filter("form[name=login]")->form([
-            "email" => "email@email.com",
+            "email" => "admin@email.com",
             "password" => "password"
         ]);
 
@@ -37,7 +37,7 @@ class LoginTest extends WebTestCase
     public function provideInvalidCredentials(): iterable
     {
         yield ["fail@email.com", "password"];
-        yield ["email@email.com", "fail"];
+        yield ["admin@email.com", "fail"];
     }
 
     /**
@@ -81,7 +81,7 @@ class LoginTest extends WebTestCase
 
         $form = $crawler->filter("form[name=login]")->form([
             "_csrf_token" => "fail",
-            "email" => "email@email.com",
+            "email" => "admin@email.com",
             "password" => "password"
         ]);
 
@@ -92,5 +92,29 @@ class LoginTest extends WebTestCase
         $client->followRedirect();
 
         $this->assertSelectorTextContains("div[name=login-form] > div.alert", "Jeton CSRF invalide.");
+    }
+
+
+    public function testIfAccountSuspended(): void
+    {
+        $client = static::createClient();
+
+        /** @var RouterInterface $router */
+        $router = $client->getContainer()->get("router");
+
+        $crawler = $client->request(Request::METHOD_GET, $router->generate("app_login"));
+
+        $form = $crawler->filter("form[name=login]")->form([
+            "email" => "suspended@email.com",
+            "password" => "password"
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertSelectorTextContains("div[name=login-form] > div.alert", "Votre compte est suspendu.");
     }
 }
